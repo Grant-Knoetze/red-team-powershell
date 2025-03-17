095247ab6212512bc4870aab4cd4c0eb1a882b34
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-03-17 21:41:33 +0200

    Initial commit
    Initial commit for shortcut.ps1.

---
+++ shortcut.ps1
@@ -0,0 +1,10 @@
+﻿$shell = New-Object -ComObject WScript.Shell
+$destination = "C:\Users\amrth\Desktop\OurShortcut.lnk"
+$shortcut = $shell.CreateShortcut($destination)
+$shortcut.IconLocation = $shortcut.TargetPath + $shortcut.IconLocation
+$originalcmd = $shortcut.TargetPath + " " + $shortcut.Arguments
+$shortcut.TargetPath = "calc.exe"
+$shortcut.Save()
+$bytes = [System.IO.File]::ReadAllBytes($destination)
+$bytes[0x15] = $bytes[0x15] -bor 0x20 #set byte 21 (0x15) bit 6 (0x20) ON
+[System.IO.File]::WriteAllBytes($destination, $bytes)
